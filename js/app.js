06c5cf9a009861d21069f75f4c718cfa64d3f634
KatKing <kathrynking.1987@gmail.com>
2016-05-29 12:23:59 -0700

    updated app to save information on localStorage as well as check for empty entries.

--- js/app.js
+++ js/app.js
@@ -1,13 +1,16 @@
+
 $(document).ready(function(){
+	
 	//Click on the check box to draw a line through the list item.
 	$('.items').on("click", ".check-box", function(){
 		$(this).parent().toggleClass("check");
 	});
 
-	$('.items').on("click", ".trash", function(event){
+	$(".items").on("click", ".trash", function(event){
 		$(this).parent().fadeOut(800, function(){
 			$(this).remove();
 			event.stopPropagation();
+			getItems();
 		});
 		
 	});
@@ -15,10 +18,34 @@ $(document).ready(function(){
 	$("input[type='text'").keypress(function(event){
 		if(event.which === 13){
 			var newItem = $(this).val();
-			$(this).val("");
+		if($(this).val().length == 0){
+			alert("Please enter a value.");
+		}else {
 			$('.items').append("<li><span class='trash'><i class='fa fa-trash'></i></span><span class='check-box'><i class='fa fa-check'></i></span>" + newItem + "</li>");
+			$(this).val("");
+			var shoppingItems = $(".items").html();
+			localStorage.setItem('shoppingItems', shoppingItems);
+			
+		}
 
 		}
 	});
 
+		if(localStorage.getItem('shoppingItems')){
+			$(".items").html(localStorage.getItem('shoppingItems'))
+		}
+
+		$("#clear").click(function(){
+			window.localStorage.clear();
+			location.reload();
+			
+		});
+
 });
+	
+
+
+	
+
+
+	
